790f231df1b2be3ddddc7f0e75597c70bb89a188
Chris_Chan <chrischandfw@gmail.com>
2022-03-03 16:38:58 -0600

    created fetch js file, added export function

---
+++ fetch-api.js
@@ -0,0 +1,9 @@
+export async function getPokedex() {
+	let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+}
+
+const resp = await fetch(https://pokedex-alchemy.herokuapp.com/api/pokedex);
+
+const json = await resp.json();
+
+return json.results;
